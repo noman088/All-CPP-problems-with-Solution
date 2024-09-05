97d50a16089034dc359ebf12da425e50839134df
Noman Ahmad <mdnoman70499@gmail.com>
2024-09-05 12:31:20 +0530

    edit some changes

--- 29_TypeCasting and Conversion/01_PremToComplex.cpp
+++ 29_TypeCasting and Conversion/01_PremToComplex.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+/*
+1.Write a C++ program to convert Primitive type to Complex type.
+Example -
+    int main()
+{
+        Complex c1;
+        Int x = 5;
+        c1 = x;
+        return 0;
+}
+*/
+
+#include <iostream>
 using namespace std;
 int main()
 {
 
-return 0 ;
+        return 0;
 }
